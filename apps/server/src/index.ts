4784e356430a33c39609067395ec52d411e3f9c1
Dongho Gang <gongdongho12@gmail.com>
2023-09-23 23:25:55 +0900

    feat: update all

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,7 @@
 import { log } from "logger";
 import { createServer } from "./server";
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const server = createServer();
 
 server.listen(port, () => {
